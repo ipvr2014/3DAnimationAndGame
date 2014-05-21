8176ae40aa3a72af694d7ea4aa5e2440c555f4ec
Cabris <christtabris@gmail.com>
2014-05-21 21:52:14 +0800

    player path

---
+++ unity_game/Assets/Scripts/PlayerPath.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerPath : MonoBehaviour
+{
+	//[SerializeField]
+	bool isMove;
+	Vector3 prePos;
+	[SerializeField] 
+	float period;
+	float count = 0;
+	public List<Vector3> path;
+	// Use this for initialization
+	void Start ()
+	{
+		prePos = transform.position;
+		path = new List<Vector3> ();
+	}
+	
+	void FixedUpdate ()
+	{
+		if (!isSamePos ()) {//diff
+			isMove = true;
+		} else {
+			isMove = false;
+		}
+		prePos = transform.position;
+	}
+	
+	void Update ()
+	{
+		if (isMove) {
+			count += Time.deltaTime;
+			if (count >= period) {
+				path.Add (transform.position);
+				count=0;
+			}
+		}else{
+			count=0;
+		}
+	}
+	
+	bool isSamePos ()
+	{
+		return transform.position.x == prePos.x &&
+			transform.position.z == prePos.z;
+	}
+}
