b2285732ebeaac74074c9af3deaa99ddcfc8b8fe
ipvr2014 <ipvr2000@gmail.com>
2014-05-22 11:10:18 +0800

    add random maze

---
+++ unity_game/Assets/Scripts/MapGeneralization.cs
@@ -0,0 +1,189 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+public class MapGeneralization : MonoBehaviour {
+
+	private const int M_SIZE = 11;
+	enum Dir {UP=0, DOWN=1, LEFT=2, RIGHT=3, NONE=4};
+	private const int CANDIDATE_ROAD = 5; // 可以開通的NODE
+	private const int ROAD = 7; // 已經開通的NODE
+	private const int WALL = 6; // 牆 (不可以走的NODE)
+	private const int DEADEND = 8; // 死路
+	
+	int[,] maze = new int[M_SIZE, M_SIZE];
+	Vector2 StartPos = new Vector2(2, 2);
+	
+	// Initialization
+	void IniMaze()
+	{
+		for(int i=0; i<M_SIZE; i++)
+		{
+			for(int j=0; j<M_SIZE; j++)
+			{
+				// in the maze
+				if(i>=2 && i<=M_SIZE-3 && j>=2 && j<=M_SIZE-3)
+				{
+					if(i%2==0 && j%2==0)
+					{
+						// 標記出可以開通的NODE
+						maze[i,j] = CANDIDATE_ROAD;
+					}
+					else
+						maze[i,j] = WALL;
+				}
+				else
+				{
+					// wall
+					maze[i,j] = WALL;
+				}
+			}
+		}
+	}
+	
+	// Ouput
+	void OutputMaze()
+	{
+		FileStream fs = new FileStream("C:\\Users\\annie\\Documents\\GitHub\\3DAnimationAndGame\\unity_game\\RandomMaze.txt", FileMode.Create);
+		StreamWriter sw = new StreamWriter(fs);
+		
+		for(int i=1; i<M_SIZE-1; i++)
+		{
+			for(int j=1; j<M_SIZE-1; j++)
+			{
+				maze[i,j] = maze[i,j] - 6;
+				
+				// 若為起點，則標記為9
+				if(i==(int)StartPos.x && j==(int)StartPos.y)
+				{
+					sw.Write("9" + " ");
+				}
+				// 避免每一列的尾端有空白字元
+				else if(j != M_SIZE-2)
+					sw.Write(maze[i,j].ToString() + " ");
+				else
+					sw.Write(maze[i,j].ToString());
+			}
+			//避免最後多了一個換行符號
+			if(i != M_SIZE-2)
+				sw.Write('\n');
+		}
+		
+		sw.Flush();
+		sw.Close();
+		fs.Close();
+		
+	}
+	
+	int FindNextNode(int x, int y)
+	{
+		bool[] direction = new bool[4];
+		bool isDeadRoad = true;
+		
+		Random.seed = System.Guid.NewGuid().GetHashCode();
+		int index = Random.Range(0, 4);
+	
+		for(int i=0; i<4; i++)
+		{
+			direction[i] = false;
+		}
+	
+		// up, down, left, right
+		if(maze[x-2, y] == CANDIDATE_ROAD)
+		{
+			direction[(int)Dir.UP] = true;
+			isDeadRoad = false;
+		}
+		if(maze[x+2, y] == CANDIDATE_ROAD)
+		{
+			direction[(int)Dir.DOWN] = true;
+			isDeadRoad = false;
+		}
+		if(maze[x, y-2] == CANDIDATE_ROAD)
+		{
+			direction[(int)Dir.LEFT] = true;
+			isDeadRoad = false;
+		}
+		if(maze[x, y+2] == CANDIDATE_ROAD)
+		{
+			direction[(int)Dir.RIGHT] = true;
+			isDeadRoad = false;
+		}
+	
+		
+		if(isDeadRoad)
+		{
+			//maze[x][y] = DEADEND;
+			return (int)Dir.NONE;
+		}
+	
+		
+		while(true)
+		{
+			if(direction[index] == true)
+				return index;
+			
+			index = Random.Range(0, 4);
+		}
+	}
+	
+	void Traversal(int x, int y)
+	{
+		int nextNode = -1;
+	
+		// visited
+		maze[x,y] = ROAD;
+	
+		nextNode = FindNextNode(x,y);
+	
+		// 當不是死路
+		while(nextNode != (int)Dir.NONE)
+		{
+			switch(nextNode)
+			{
+			case (int)Dir.UP:
+				maze[x-1, y] = ROAD;
+				Traversal(x-2, y);
+				break;
+	
+			case (int)Dir.DOWN:
+				maze[x+1, y] = ROAD;
+				Traversal(x+2, y);
+				break;
+	
+			case (int)Dir.LEFT:
+				maze[x, y-1] = ROAD;
+				Traversal(x, y-2);
+				break;
+	
+			case (int)Dir.RIGHT:
+				maze[x, y+1] = ROAD;
+				Traversal(x, y+2);
+				break;
+	
+			default:
+				break;
+			}
+			nextNode = FindNextNode(x,y);
+		}
+	
+	}
+	
+	
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		IniMaze();
+		Traversal((int)StartPos.x, (int)StartPos.y);
+		OutputMaze();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ unity_game/Assets/Scripts/WallController.js
@@ -0,0 +1,71 @@
+﻿
+import System.IO;
+
+var test_prefab1 : GameObject;
+var start_prefab : GameObject;
+var terrain : GameObject;
+
+var wall_side_length : float = 2;
+
+function Start ()
+{
+	//***** 取得 terrain 的長跟寬 *****//
+	terrain = GameObject.Find("Plane");
+	//Debug.Log(terrain.name);
+	var size_x : float = terrain.collider.bounds.size.x;
+	var scale_x : float = terrain.transform.localScale.x;
+	
+	var size_z : float = terrain.collider.bounds.size.z;
+	var scale_z : float = terrain.transform.localScale.z;
+	
+	var MapWidth : float = size_x * scale_x;
+	var MapHeight : float = size_z * scale_z;
+	
+	
+	//***** 取得 wall 的長跟寬 *****//
+	// ????
+	
+	//***** 迷宮初始產生的位置 *****//
+	
+    //theSourceFile = System.IO.FileInfo("Data.txt");
+    transform.position.x = 0/*wall_side_length/2*/ ;
+	transform.position.y = 1;
+	transform.position.z = 0/*MapHeight - wall_side_length/2*/;
+	
+	var theSourceFile : StreamReader = new System.IO.StreamReader("RandomMaze.txt");
+	//Debug.Log(theSourceFile.CurrentEncoding);
+	var line : String;
+	
+	while (!theSourceFile.EndOfStream)
+	{
+		line = theSourceFile.ReadLine();
+		var split : String[] = line.Split(" "[0]);
+	
+		for(var s in split)
+		{
+			var str2int : int = parseInt(s);
+			transform.position.x += wall_side_length ;
+			
+			// WALL
+			if(str2int == 0)
+			{
+		  		//print("x");
+		  		var CubeClone1: GameObject = Instantiate(test_prefab1,transform.position,transform.rotation);
+			}
+			// ROAD
+			else if(str2int == 1)
+			{
+		  		//print("o");
+			}
+			// Start Position
+			else if(str2int == 9)
+			{
+				var StartPoint : GameObject = Instantiate(start_prefab, transform.position, transform.rotation);
+			}
+		}
+		Debug.Log(line);
+		//nextline
+		transform.position.z -= wall_side_length;
+		transform.position.x = wall_side_length/2 ;//reset xx
+	}
+}
